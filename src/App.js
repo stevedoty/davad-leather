da2ea6ceee894d1326efe9add24ec0789227cf0f
Steve Doty <stevedoty16@gmail.com>
2020-04-26 19:07:47 -0700

    basic homepage layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import HomePage from './pages/homepage/homepage.js';
+
 import './App.css';
 
 class App extends React.Component {
@@ -7,11 +10,11 @@ class App extends React.Component {
     const navbar=["Home", "About", "Contact", "Cart"]
       return (
         <div className="App">
-            <div className="navbar">{ navbar.map( item => <div>item</div>)}</div>
-            <div className="home-page"></div>
+            <div className="navbar">{ navbar.map( item => <div>{item}</div>)}</div>
+            <HomePage/>
             <div className="about-page"></div>
             <div className="contact-page"></div>
-            <div className="cart-page"></div>
+            <div className="checkout-page"></div>
         </div>
       );
     }
---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+
+
+
+const HomePage = () => (
+    <div className="home-page">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="background"></div>
+                <div className="content">
+                    <h1 className="title">title</h1>
+                    <span className="subtitle">subtitle</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="background"></div>
+                <div className="content">
+                    <h1 className="title">title</h1>
+                    <span className="subtitle">subtitle</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="background"></div>
+                <div className="content">
+                    <h1 className="title">title</h1>
+                    <span className="subtitle">subtitle</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="background"></div>
+                <div className="content">
+                    <h1 className="title">title</h1>
+                    <span className="subtitle">subtitle</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="background"></div>
+                <div className="content">
+                    <h1 className="title">title</h1>
+                    <span className="subtitle">subtitle</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="background"></div>
+                <div className="content">
+                    <h1 className="title">title</h1>
+                    <span className="subtitle">subtitle</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
