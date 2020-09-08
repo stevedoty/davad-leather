ef3551911f604fc2c0c6c72fa859dbd6dee94b9b
Steve Doty <stevedoty16@gmail.com>
2020-09-08 14:15:57 -0700

    landing page image done

--- src/pages/landingpage/landingpage.js
+++ src/pages/landingpage/landingpage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-
-
+import landingPageImage from '../../assets/landing-page-image.jpg';
 
 const LandingPage = () => (
-    <div id="landing-page">
+    <div id="landing-page"
+      style={{
+        backgroundImage: `url(${landingPageImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center"
+       }}>
         <div className="summary">
             <h1>Davad Leather</h1>
             <h5>Handmade One of a Kind Leatherwork</h5>
